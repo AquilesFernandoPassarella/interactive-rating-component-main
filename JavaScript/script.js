2a15ef57896f48ea95303a80ce93e144667c43b5
Aquiles Fernando Passarella <aquilesfernandop@hotmail.com>
2023-05-05 01:17:58 -0300

    Adicionando animação no click do botão

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,14 @@
 const btn = document.getElementById("btn");
-const normal = document.getElementsByClassName("avaliacao");
+const estilo = document.getElementsByClassName("container");
 const message = document.getElementsByClassName("message__final");
 
 
+ btn.onclick = function(){
 
+    estilo[0].style.display = "none";
+    message[0].style.display = "flex";
+    
+ }
+   
+ 
 

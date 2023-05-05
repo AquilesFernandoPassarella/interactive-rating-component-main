1d0bd77db1d4457d42eface5d4d149b02550d03d
Aquiles Fernando Passarella <aquilesfernandop@hotmail.com>
2023-05-05 01:55:54 -0300

    Commit final

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,14 +1,43 @@
 const btn = document.getElementById("btn");
 const estilo = document.getElementsByClassName("container");
 const message = document.getElementsByClassName("message__final");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const btn5 = document.getElementById("btn5");
+let nota = document.getElementById("selecionado");
 
+btn.addEventListener("click", () => {
+  estilo[0].style.display = "none";
+  message[0].style.display = "flex";
+});
 
- btn.onclick = function(){
+btn1.addEventListener("click", (event) => {
+  let valor = event.target.value;
+  exibirEscolha(valor);
+});
 
-    estilo[0].style.display = "none";
-    message[0].style.display = "flex";
-    
- }
-   
- 
+btn2.addEventListener("click", (event) => {
+  let valor = event.target.value;
+  exibirEscolha(valor);
+});
 
+btn3.addEventListener("click", (event) => {
+  let valor = event.target.value;
+  exibirEscolha(valor);
+});
+
+btn4.addEventListener("click", (event) => {
+  let valor = event.target.value;
+  exibirEscolha(valor);
+});
+
+btn5.addEventListener("click", (event) => {
+  let valor = event.target.value;
+  exibirEscolha(valor);
+});
+
+function exibirEscolha(valor) {
+  nota.innerHTML = valor;
+}
